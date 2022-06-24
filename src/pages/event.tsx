1008579b6e77dac836a619af8bc8a938f7688a68
LuizFelipe883 <luizfelipedesouza94@gmail.com>
2022-06-24 13:10:02 -0300

    Aula 02

---
+++ src/pages/event.tsx
@@ -0,0 +1,17 @@
+import { Header } from "../components/header";
+import { Sidebar } from "../components/sidebar";
+import { Video } from "../components/video";
+
+export function Event() {
+    return (
+        <div className=" flex flex-col min-h-screen">
+            <Header />
+            <main className="flex flex-1">
+                <Video />
+                <Sidebar />
+            </main>
+
+        </div>
+
+    )
+}
